29116a8d4e72d84b4aff322f36d9bb334faeaa67
Jamie Dorst <Jamie@Jamie.local>
2020-11-08 16:10:32 -0500

    test

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,92 +1,113 @@
-import React, {Component} from 'react';
-import './css/master.css';
-import jamie from '../images/jamie.jpg';
-import andy from '../images/andy.jpg';
-import jesse from '../images/jesse.jpg';
+import React, { Component } from "react";
+import "./css/master.css";
+import jamie from "../images/jamie.jpg";
+import andy from "../images/andy.jpg";
+import jesse from "../images/jesse.jpg";
 
 type State = {};
 type Props = {};
 
-class About extends Component < State, Props > {
+class About extends Component<State, Props> {
+  render() {
+    return (
+      <div>
+        <div className="col-12 about">
+          <h1 className="about">About</h1>
+        </div>
 
-    render() {
-        return (<div>
+        <div className="col-5 about">
+          <p className="about">
+            The CMU Course Advisor was created by balfdjkljk fsjdkls
+            fsdfjdklsjfkl jfkdlsjf dk jskld dkk kdlskld klk dskl kldsklkdls
+            fjkdls. jflsjkl sd. ildsjflksdjkl ekl jfkdls jkdslj leowji ji Ie jil
+            jfklJ LKJKL fjkls jklds eio jlsfjkl. jIJILSJ lfjifljeio i JKLAJ lkf
+            jisljil JLA JIlfjsidlf jkdls fsd.fj iosfj skldj fkdlsjflkjdkslje
+            iowjfiowj! thank you for using it. it is relaly nice of you. i hope
+            you ike it. i ohpe it works. thats all. this is kinda long but
+            hopeuflly isomething wil l fill it. who know.
+          </p>
+        </div>
 
-            <div className="col-12 about">
-                <h1 className="about">About</h1>
-            </div>
+        <div className="col-2 about">
+          <div>
+            <img
+              className="pics"
+              src={jamie}
+              onError="this.style.borderWidth=0"
+              alt="Jamie Dorst"
+            />
+          </div>
+          <div>
+            <h3 className="about">JAMIE DORST</h3>
+          </div>
+          <div>
+            <p className="mini about">Designer &#38; Front-End Developer</p>
+          </div>
+          <div>
+            <p className="aboutDetail">
+              Jamie is currently a sophomore at CMU studying Human-Computer
+              Interaction and Psychology. She is also doing more stuff so here
+              is more about that. This is interesting right&#63; I keep using
+              apostrophes and it is making my react text color weirdly in
+              sublime. Now I took them out so it would not be so weird.
+            </p>
+          </div>
+        </div>
 
-            <div className="col-5 about">
-                <p className ="about">The CMU Course Advisor was created by balfdjkljk fsjdkls
-                                      fsdfjdklsjfkl jfkdlsjf dk jskld dkk kdlskld klk dskl kldsklkdls
-                                      fjkdls. jflsjkl sd. ildsjflksdjkl ekl jfkdls jkdslj leowji ji Ie
-                                      jil jfklJ LKJKL fjkls jklds eio jlsfjkl. jIJILSJ lfjifljeio i
-                                      JKLAJ lkf jisljil JLA JIlfjsidlf jkdls fsd.fj iosfj skldj
-                                      fkdlsjflkjdkslje iowjfiowj! thank you for using it. it is relaly
-                                      nice of you. i hope you ike it. i ohpe it works. thats all. this
-                                      is kinda long but hopeuflly isomething wil l fill it. who know.</p>
-            </div>
+        <div className="col-2 about">
+          <div>
+            <img
+              className="pics"
+              src={andy}
+              onError="this.style.borderWidth=0"
+              alt="Andy Chen"
+            />
+          </div>
+          <div>
+            <h3 className="about">ANDY CHEN</h3>
+          </div>
+          <div>
+            <p className="mini about">Back-End Developer</p>
+          </div>
+          <div>
+            <p className="aboutDetail">
+              Andy is currently a sophomore at CMU studying Information Systems.
+              He is also doing more stuff so here is more about that. This is
+              interesting right&#63; I keep using apostrophes and it is making
+              my react text color weirdly in sublime. Now I took them out so it
+              would not be so weird.
+            </p>
+          </div>
+        </div>
 
-            <div className="col-2 about">
-                <div>
-                    <img className="pics" src={jamie} onError="this.style.borderWidth=0" alt="Jamie Dorst"/>
-                </div>
-                <div>
-                    <h3 className="about">JAMIE DORST</h3>
-                </div>
-                <div>
-                    <p className="mini about">Designer &#38; Front-End Developer</p>
-                </div>
-                <div>
-                    <p className="aboutDetail">Jamie is currently a sophomore at CMU studying Human-Computer
-                                               Interaction and Psychology. She is also doing more stuff so
-                                               here is more about that. This is interesting right&#63; I keep using
-                                               apostrophes and it is making my react text color weirdly in sublime.
-                                               Now I took them out so it would not be so weird.</p>
-                </div>
-            </div>
-
-            <div className="col-2 about">
-                <div>
-                    <img className="pics" src={andy} onError="this.style.borderWidth=0" alt="Andy Chen"/>
-                </div>
-                <div>
-                    <h3 className="about">ANDY CHEN</h3>
-                </div>
-                <div>
-                    <p className="mini about">Back-End Developer</p>
-                </div>
-                <div>
-                    <p className="aboutDetail">Andy is currently a sophomore at CMU studying Information Systems.
-                                               He is also doing more stuff so here is more about that. This
-                                               is interesting right&#63; I keep using apostrophes and it is
-                                               making my react text color weirdly in sublime. Now I took
-                                               them out so it would not be so weird.</p>
-                </div>
-            </div>
-
-            <div className="col-3 about">
-                <div>
-                    <img className="pics" src={jesse} onError="this.style.borderWidth=0" alt="Jesse Chan"/>
-                </div>
-                <div>
-                    <h3 className="about">JESSE CHAN</h3>
-                </div>
-                <div>
-                    <p className="mini about">Back-End Developer</p>
-                </div>
-                <div>
-                    <p className="aboutDetail">Jesse is currently a sophomore at CMU studying Electrical and
-                                               Computer Engineering and Robotics. He is also doing more stuff,
-                                               here is more about that. This is interesting right&#63; I keep using
-                                               apostrophes and it is making my react text color weirdly in
-                                               sublime. Now I took them out so it would not be so weird.</p>
-                </div>
-            </div>
-
-        </div>);
-    }
-
+        <div className="col-3 about">
+          <div>
+            <img
+              className="pics"
+              src={jesse}
+              onError="this.style.borderWidth=0"
+              alt="Jesse Chan"
+            />
+          </div>
+          <div>
+            <h3 className="about">JESSE CHAN</h3>
+          </div>
+          <div>
+            <p className="mini about">Back-End Developer</p>
+          </div>
+          <div>
+            <p className="aboutDetail">
+              Jesse is currently a sophomore at CMU studying Electrical and
+              Computer Engineering and Robotics. He is also doing more stuff,
+              here is more about that. This is interesting right&#63; I keep
+              using apostrophes and it is making my react text color weirdly in
+              sublime. Now I took them out so it would not be so weird.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
--- client/src/components/searchResults.js
+++ client/src/components/searchResults.js
@@ -1,76 +1,106 @@
 // search function
-import React, {Component} from 'react';
-import './css/master.css';
+import React, { Component } from "react";
+import "./css/master.css";
 
 type State = {};
 type Props = {};
 
 function Results(props) {
-    return(<div>
-            
-            <div className="col-12 divider">
-                <div id="divider"></div>
-            </div>
-            <div className="col-7 search">
-                <div className="title">
-                    <h3 className="search">00-000</h3>
-                    <p className="search">Probably Lengthy CMU Class Name</p>
-                </div>
-                <p className="mini search">SCS: Computer Science</p>
-            </div>
+  return (
+    <div>
+      <div className="col-12 divider">
+        <div id="divider"></div>
+      </div>
+      <div className="col-7 search">
+        <div className="title">
+          <h3 className="search">00-001</h3>
+          <p className="search">Probably Lengthy CMU Class Name</p>
+        </div>
+        <p className="mini search">SCS: Computer Science</p>
+      </div>
 
-            <div className="col-3 search">
-                <h4 className="search">professor one, professor two</h4>
-                <p className="mini search">Offered Fall 2019</p>
-            </div>
+      <div className="col-3 search">
+        <h4 className="search">professor one, professor two</h4>
+        <p className="mini search">Offered Fall 2019</p>
+      </div>
 
-            <div className="col-2 search">
-                <h5 className="search">4.6<span className="outOf">&#47;5</span></h5>
-            </div>
-
-        </div>);
+      <div className="col-2 search">
+        <h5 className="search">
+          4.6<span className="outOf">&#47;5</span>
+        </h5>
+      </div>
+    </div>
+  );
 }
 
-class SearchResults extends Component < State, Props > {
+class SearchResults extends Component<State, Props> {
+  render() {
+    let arr = [
+      <Results />,
+      <Results />,
+      <Results />,
+      <Results />,
+      <Results />,
+      <Results />,
+      <Results />,
+      <Results />,
+    ];
 
-    render() {
-        let arr = [ <Results/>, <Results/>, <Results/>, <Results/>, <Results/>, <Results/>, <Results/>, <Results/>]
+    return (
+      <div>
+        <div className="col-12 search">
+          <h1 className="search">&ldquo;search query&rdquo;</h1>
+        </div>
 
-        return (<div>
-            <div className="col-12 search">
-                <h1 className="search">&ldquo;search query&rdquo;</h1>
-            </div>
+        <div className="col-8 filter">
+          <div className="button">
+            <h6 className="search">
+              ALL SEMESTERS<span className="triangle"></span>
+            </h6>
+          </div>
+          <div className="button">
+            <h6 className="search">
+              ALL PROFESSORS<span className="triangle"></span>
+            </h6>
+          </div>
+          <div className="button">
+            <h6 className="search">
+              ALL RATINGS<span className="triangle"></span>
+            </h6>
+          </div>
+          <div className="button">
+            <h6 className="search">
+              ALL REQUIREMENTS<span className="triangle"></span>
+            </h6>
+          </div>
+          <div className="button">
+            <h6 className="search">
+              ALL DEPARTMENTS<span className="triangle"></span>
+            </h6>
+          </div>
+          <div className="button">
+            <h6 className="search">
+              ALL UNITS<span className="triangle"></span>
+            </h6>
+          </div>
+        </div>
 
-            <div className="col-8 filter">
-                <div className="button">
-                    <h6 className="search">ALL SEMESTERS<span className="triangle"></span></h6>
-                </div>
-                <div className="button">
-                    <h6 className="search">ALL PROFESSORS<span className="triangle"></span></h6>
-                </div>
-                <div className="button">
-                    <h6 className="search">ALL RATINGS<span className="triangle"></span></h6>
-                </div>
-                <div className="button">
-                    <h6 className="search">ALL REQUIREMENTS<span className="triangle"></span></h6>
-                </div>
-                <div className="button">
-                    <h6 className="search">ALL DEPARTMENTS<span className="triangle"></span></h6>
-                </div>
-                <div className="button">
-                    <h6 className="search">ALL UNITS<span className="triangle"></span></h6>
-                </div>
-            </div>
+        <div className="col-4 sort">
+          <p className="mini search">
+            00 results sorted by <span id="sort">Best Match</span>
+          </p>
+        </div>
 
-            <div className="col-4 sort">
-                <p className="mini search">00 results sorted by <span id="sort">Best Match</span></p>
-            </div>
-
-            <div onClick={()=>{window.location = '/courseDetail'}}>
-                {arr}
-            </div>
-        </div>);
-    }
+        <div
+          onClick={() => {
+            window.location = "/courseDetail";
+          }}
+        >
+          {arr}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchResults;
